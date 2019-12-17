89782a1aa26a65da6ae86b3b912a8648c2bc0ceb
Junuu <37577891+Junuu@users.noreply.github.com>
2019-12-17 17:20:33 +0900

    가격표 수정

--- Overwatch.js
+++ Overwatch.js
@@ -15,17 +15,22 @@ var p4450to4500;
 //가격을 4개로 분리하여 함수를 통해 변수에 값 저장
 function set_price_team()
 {
-  p0to2000=4500;
-  p2000to2500=5500;
-  p2500to3000=6000;
-  p3000to3250=7000;
-  p3250to3500=8500;
-  p3500to3750=12000;
-  p3750to4000=16000;
-  p4000to4100=25000;
-  p4100to4200=40000;
-  p4200to4300=70000;
-  p4300to4400=100000;
+    p0to2000=4000;
+    p2000to2500=5000;
+    p2500to3000=5500;
+    p3000to3250=6500;
+    p3250to3500=8000;
+    p3500to3750=11000;
+    p3750to4000=15000;
+    p4000to4100=25000;
+    p4100to4200=35000;
+    p4200to4300=65000;
+    p4300to4400=80000;
+    p4400to4450=110000;
+    p4450to4500=110000;
+    p4500to4550=300000;
+    p4550to4600=600000;
+
 }
 
 function set_price_not_team()
@@ -65,85 +70,83 @@ function set_price_not_team_duo()
   p3500to3750=13500;
   p3750to4000=18000;
 }
+
 function calculate(startscore,finishscore,check_team,check_solo)
 {
   var price=0;
   //시작 점수가 마지막점수와 같지 않을 때 까지 startscore를 하나씩 더해가며 점수를 검사하여 price값을 갱신
   while(startscore!=finishscore)
   {
-    if(startscore<2000)
-    {
-      price=price+(p0to2000/50);
-    }
-    else if(startscore<2500)
-    {
-      price=price+(p2000to2500/50);
-    }
-    else if(startscore<3000)
-    {
-      price=price+(p2500to3000/50);
-    }
-    else if(startscore<3250)
-    {
-      price=price+(p3000to3250/50);
-    }
-    else if(startscore<3500)
-    {
-      price=price+(p3250to3500/50);
-    }
-    else if(startscore<3750)
-    {
-      price=price+(p3500to3750/50);
-    }
-    else if(startscore<4000)
-    {
-      price=price+(p3750to4000/50);
-    }
-    else if(startscore<4100)
-    {
-      if(check_solo=="듀오") //듀오의 경우 4000점까지의 가격밖에 설정이 안되있으므로 4000점까지 계산
+      if (startscore < 2000) {
+          price = price + (p0to2000 / 50);
+      }
+      else if (startscore < 2500) {
+          price = price + (p2000to2500 / 50);
+      }
+      else if (startscore < 3000) {
+          price = price + (p2500to3000 / 50);
+      }
+      else if (startscore < 3250) {
+          price = price + (p3000to3250 / 50);
+      }
+      else if (startscore < 3500) {
+          price = price + (p3250to3500 / 50);
+      }
+      else if (startscore < 3750) {
+          price = price + (p3500to3750 / 50);
+      }
+      else if (startscore < 4000) {
+          price = price + (p3750to4000 / 50);
+      }
+      else if (startscore < 4100) {
+          if (check_solo == "듀오") //듀오의 경우 4000점까지의 가격밖에 설정이 안되있으므로 4000점까지 계산
+          {
+              alert("[ERROR: 계산허용범위 초과] 4000까지 계산된 가격을 반환합니다");
+              return price;
+          }
+          price = price + (p4000to4100 / 50);
+      }
+      else if (startscore < 4200) {
+          price = price + (p4100to4200 / 50);
+      }
+      else if (startscore < 4200) {
+          price = price + (p4100to4200 / 50);
+      }
+      else if (startscore < 4300) {
+          price = price + (p4200to4300 / 50);
+      }
+      else if (startscore < 4400) {
+          price = price + (p4300to4400 / 50);
+      }
+      else if (startscore < 4450) {
+          price = price + (p4400to4450 / 50);
+      }
+      else if (startscore < 4500) {
+          price = price + (p4450to4500 / 50);
+      }
+      else if (startscore < 4550)//4500점 까지 가격설정이 안되있기 떄문에 4500점 까지 계산
       {
-        alert("[ERROR: 계산허용범위 초과] 4000까지 계산된 가격을 반환합니다");
-        return price;
+          if (check_team == "기사") {
+              alert("[ERROR: 계산허용범위 초과] 4500까지 계산된 가격을 반환합니다");
+              return price;
+          }
+          else
+          {
+              price = price + (p4500to4550/50);
+          }
+          
       }
-      price=price+(p4000to4100/50);
-    }
-    else if(startscore<4200)
-    {
-      price=price+(p4100to4200/50);
-    }
-    else if(startscore<4200)
-    {
-      price=price+(p4100to4200/50);
-    }
-    else if(startscore<4300)
-    {
-      price=price+(p4200to4300/50);
-    }
-    else if(startscore<4400)
-    {
-      price=price+(p4300to4400/50);
-    }
-    else if(startscore<4450)
-    {
-      if(check_team =="팀장") //팀장의 경우 4400까지밖에 값이 입력안되있기 때문에 4400까지 계산
+      else if (startscore < 4600)
       {
-        alert("[ERROR: 계산허용범위 초과] 4400까지 계산된 가격을 반환합니다");
-        return price;
+          price = price + (p4550to4600 / 50);
       }
-      else {
-        price=price+(p4400to4450/50);
+      else
+      {
+          
+              alert("[ERROR: 계산허용범위 초과] 4600까지 계산된 가격을 반환합니다");
+              return price;
+          
       }
-    }
-    else if(startscore<4500)
-    {
-      price=price+(p4450to4500/50);
-    }
-    else //4500점 까지 가격설정이 안되있기 떄문에 4500점 까지 계산
-    {
-      alert("[ERROR: 계산허용범위 초과] 4500까지 계산된 가격을 반환합니다");
-      return price;
-    }
     startscore = startscore + 1;
   }
   return price;
